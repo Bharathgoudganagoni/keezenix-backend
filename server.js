afd6c04c5815444a46862bccfbf94eef353e6397
Bharathgoudganagoni <bharathgoudganagoni123@gmail.com>
2026-06-22 17:42:20 +0530

    .env update

--- server.js
+++ server.js
@@ -54,9 +54,8 @@ const uploadMiddleware = upload.single("resume");
 // ✅ Email config (Render/Gmail compatible)
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
